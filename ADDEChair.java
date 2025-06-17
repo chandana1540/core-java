99924d15c5fb4648e3afc0ade77b03c964219bbe
chandana <you@example.com>
2025-06-17 14:06:00 +0530

    sop

---
+++ ADDEChair.java
@@ -0,0 +1,6 @@
+class ADDEChair {
+
+      public static void main( String headsets[]) { 
+      System.out.println("With stackable chairs you'll always have a way to seat all your guests.  ");
+}
+}
