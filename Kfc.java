fd620a15f8f26488fc0c2f853f23d67c9485f705
chandana <you@example.com>
2025-07-16 19:41:56 +0530

    message

--- Kfc.java
+++ Kfc.java
@@ -1,70 +1,203 @@
 class Kfc{
-	public static double getmealPrice(String mealName){
+	public static double search(String mealName){
     double Price = 0.0;
     if(mealName == "Wednesday Bucket"){
     Price = 699.05;
+	return Price;
     }else if(mealName == "Strips Bucket"){
     Price = 350.48;
+	return Price;
     }else if(mealName == "5pc Leg peice Bucket"){
     Price = 628.57;
+	return Price;
     }else if(mealName == "10 pc Leg Peice Bucket"){
     Price = 628.57;
+	return Price;
     }else if(mealName == "Stay Home Bucket"){
     Price = 788.57;
+	return Price;
     }else if(mealName == "2Dips leg"){
     Price = 515.24;
+	return Price;
     }else if(mealName == "Family Fest"){
     Price = 828.57;
+	return Price;
     }else if(mealName == "Ultimate Savings Bucket"){
     Price = 699.05;
+	return Price;
     }else if(mealName == "Big 12"){
     Price = 719.05;
+	return Price;
     }else if(mealName == "Mingles Bucket"){
-    Price = 345.09;	
+    Price = 345.09;
+    return Price;	
 	}else if(mealName == "8pc Hot Crispy And Chicken"){
     Price = 437.00;
+	return Price;
 	}else if(mealName == "Zinger Bucket"){
     Price = 340.09;
+	return Price;
 	}else if(mealName == "Twister Bucket"){
     Price = 230.10;
+	return Price;
 	}else if(mealName == "Double Down Burger"){
     Price = 628.57;
+	return Price;
 	}else if(mealName == "Zinger Burger"){
     Price = 200.08;
+	return Price;
 	}else if(mealName == "Chilli Chicken burger"){
     Price = 300.00;
+	return Price;
 	}else if(mealName == "Veg Zinger"){
     Price = 150.00;
+	return Price;
 	}else if(mealName == "Veg Burger"){
     Price = 135.00;
+	return Price;
 	}else if(mealName == "Double Chicken Roll"){
     Price = 120.00;
+	return Price;
 	}else if(mealName == "Class Chicken Roll"){
     Price = 250.00;
+	return Price;
 	}else if(mealName == "Ricz Bowlz"){
     Price = 130.00;
+	return Price;
 	}else if(mealName == "pepsi"){
     Price = 125.00;
+	return Price;
 	}else if(mealName == "Mountain Dew"){
     Price = 320.00;
+	return Price;
 	}else if(mealName == "Soft Drinks"){
     Price = 240.87;
+	return Price;
 	}else if(mealName == "Milk Shakes"){
     Price = 230.00;
+	return Price;
 	}else if(mealName == "Brownies"){
     Price = 30.00;
+	return Price;
 	}else if(mealName == "Chicken Strips"){
     Price = 40.00;
+	return Price;
 	}else if(mealName == "Hot Wings"){
     Price = 80.00;
+	return Price;
 	}else if(mealName == "peri peri Chicken"){
     Price = 96.00;
+	return Price;
 	}else if(mealName == "Smoky Red Chicken"){
     Price = 45.00;
+	return Price;
 	}else if(mealName == "Popcorn Chicken"){
     Price = 90.00;
+	return Price;
 	}else if(mealName == "Onion Rings"){
     Price = 60.00;
+	return Price;
+	} else System.out.println(mealName + "is Invalid");
+	return Price;
+	}
+public static double search(String mealName, int quantity){
+    double Price = 0.0;
+    if(mealName == "Wednesday Bucket"){
+    Price = 699.05*quantity;
+	return Price;
+    }else if(mealName == "Strips Bucket"){
+    Price = 350.48*quantity;
+	return Price;
+    }else if(mealName == "5pc Leg peice Bucket"){
+    Price = 628.57*quantity;
+	return Price;
+    }else if(mealName == "10 pc Leg Peice Bucket"){
+    Price = 628.57*quantity;
+	return Price;
+    }else if(mealName == "Stay Home Bucket"){
+    Price = 788.57*quantity;
+	return Price;
+    }else if(mealName == "2Dips leg"){
+    Price = 515.24*quantity;
+	return Price;
+    }else if(mealName == "Family Fest"){
+    Price = 828.57*quantity;
+	return Price;
+    }else if(mealName == "Ultimate Savings Bucket"){
+    Price = 699.05*quantity;
+	return Price;
+    }else if(mealName == "Big 12"){
+    Price = 719.05*quantity;
+	return Price;
+    }else if(mealName == "Mingles Bucket"){
+    Price = 345.09*quantity;
+    return Price;	
+	}else if(mealName == "8pc Hot Crispy And Chicken"){
+    Price = 437.00*quantity;
+	return Price;
+	}else if(mealName == "Zinger Bucket"){
+    Price = 340.09*quantity;
+	return Price;
+	}else if(mealName == "Twister Bucket"){
+    Price = 230.10*quantity;
+	return Price;
+	}else if(mealName == "Double Down Burger"){
+    Price = 628.57*quantity;
+	return Price;
+	}else if(mealName == "Zinger Burger"){
+    Price = 200.08*quantity;
+	return Price;
+	}else if(mealName == "Chilli Chicken burger"){
+    Price = 300.00*quantity;
+	return Price;
+	}else if(mealName == "Veg Zinger"){
+    Price = 150.00*quantity;
+	return Price;
+	}else if(mealName == "Veg Burger"){
+    Price = 135.00*quantity;
+	return Price;
+	}else if(mealName == "Double Chicken Roll"){
+    Price = 120.00*quantity;
+	return Price;
+	}else if(mealName == "Class Chicken Roll"){
+    Price = 250.00*quantity;
+	return Price;
+	}else if(mealName == "Ricz Bowlz"){
+    Price = 130.00*quantity;
+	return Price;
+	}else if(mealName == "pepsi"){
+    Price = 125.00*quantity;
+	return Price;
+	}else if(mealName == "Mountain Dew"){
+    Price = 320.00*quantity;
+	return Price;
+	}else if(mealName == "Soft Drinks"){
+    Price = 240.87*quantity;
+	return Price;
+	}else if(mealName == "Milk Shakes"){
+    Price = 230.00*quantity;
+	return Price;
+	}else if(mealName == "Brownies"){
+    Price = 30.00*quantity;
+	return Price;
+	}else if(mealName == "Chicken Strips"){
+    Price = 40.00*quantity;
+	return Price;
+	}else if(mealName == "Hot Wings"){
+    Price = 80.00*quantity;
+	return Price;
+	}else if(mealName == "peri peri Chicken"){
+    Price = 96.00*quantity;
+	return Price;
+	}else if(mealName == "Smoky Red Chicken"){
+    Price = 45.00*quantity;
+	return Price;
+	}else if(mealName == "Popcorn Chicken"){
+    Price = 90.00*quantity;
+	return Price;
+	}else if(mealName == "Onion Rings"){
+    Price = 60.00*quantity;
+	return Price;
 	} else System.out.println(mealName + "is Invalid");
 	return Price;
 	}
--- SwiggyApp.java
+++ SwiggyApp.java
@@ -1,66 +1,191 @@
 class SwiggyApp {
-	public static double  getfoodCost(String foodName){
+	public static double search(String foodName){
 	double Cost = 0.0;
         if(foodName == "Mini Idli"){
 		Cost = 79.00;
+		return Cost;
 	    }else if(foodName == "Butter Masala Dosa"){
 		Cost = 95.00;
+		return Cost;
 	    }else if(foodName == "Bun Butter Jam"){
 		Cost = 30.00;
+		return Cost;
 	    }else if(foodName == "Tea"){
 		Cost = 30.00;
+		return Cost;
 		}else if(foodName == "Coffee"){
 		Cost = 40.00;
+		return Cost;
 		}else if(foodName == "Small Samosa"){
 		Cost = 20.00;
+		return Cost;
 		}else if(foodName == "Plain Dosa"){
 		Cost = 60.00;
+		return Cost;
 		}else if(foodName == "Onion Pakoda"){
 		Cost = 50.00;
+		return Cost;
 		}else if(foodName == "Puri"){
 		Cost = 45.00;
+		return Cost;
 		}else if(foodName == "Veg Puff"){
 		Cost = 69.00;
+		return Cost;
 		}else if(foodName == "Masala Puri"){
 		Cost = 55.00;
+		return Cost;
 		}else if(foodName == "Sweet Lassi"){
 		Cost = 70.00;
+		return Cost;
 		}else if(foodName == "Filter Coffee"){
 		Cost = 40.00;
+		return Cost;
 		}else if(foodName == "Plain Rava Idli"){
 		Cost = 40.00;
+		return Cost;
 		}else if(foodName == "Lemon Tea"){
 		Cost = 79.00;
+		return Cost;
 		}else if(foodName == "Small Vada"){
 		Cost = 20.00;
+		return Cost;
 		}else if(foodName == "Veg Noodles"){
 		Cost = 99.00;
+		return Cost;
 		}else if(foodName == "Veg Fried Rice"){
 		Cost = 99.00;
+		return Cost;
 		}else if(foodName == "Chilli Chicken"){
 		Cost = 95.00;
+		return Cost;
 		}else if(foodName == "Chicken Biryani"){
 		Cost = 92.00;
+		return Cost;
 		}else if(foodName == "Chicken65"){
 		Cost = 140.00;
+		return Cost;
 		}else if(foodName == "Veg Biryani"){
 		Cost = 169.00;
+		return Cost;
 		}else if(foodName == "Noodles"){
 		Cost = 120.00;
+		return Cost;
 		}else if(foodName == "Fried Rice"){
 		Cost = 130.00;
+		return Cost;
 		}else if(foodName == "Paneer Butter Masala"){
 		Cost = 150.00;
+		return Cost;
 		}else if(foodName == "Dal Makhani"){
 		Cost = 140.00;
+		return Cost;
 		}else if(foodName == "Gobi Manchurian"){
 		Cost = 130.00;
+		return Cost;
 		}else if(foodName == "Mutton Biryani"){
 		Cost = 200.00;
+		return Cost;
 		}else if(foodName == "Boiled Egg"){
 		Cost = 15.00;
+		return Cost;
 		}else if(foodName == "Thali"){
-		Cost = 150.00;	
+		Cost = 150.00;
+        return Cost;		
+	    }else System.out.println(foodName + "is Invalid");
+	    return Cost;
+	    }
+public static double search(String foodName, int quantity){
+	double Cost = 0.0;
+        if(foodName == "Mini Idli"){
+		Cost = 79.00;
+		return Cost*quantity;
+	    }else if(foodName == "Butter Masala Dosa"){
+		Cost = 95.00;
+		return Cost*quantity;
+	    }else if(foodName == "Bun Butter Jam"){
+		Cost = 30.00;
+		return Cost*quantity;
+	    }else if(foodName == "Tea"){
+		Cost = 30.00;
+		return Cost*quantity;
+		}else if(foodName == "Coffee"){
+		Cost = 40.00;
+		return Cost*quantity;
+		}else if(foodName == "Small Samosa"){
+		Cost = 20.00;
+		return Cost*quantity;
+		}else if(foodName == "Plain Dosa"){
+		Cost = 60.00;
+		return Cost*quantity;
+		}else if(foodName == "Onion Pakoda"){
+		Cost = 50.00;
+		return Cost*quantity;
+		}else if(foodName == "Puri"){
+		Cost = 45.00;
+		return Cost*quantity;
+		}else if(foodName == "Veg Puff"){
+		Cost = 69.00;
+		return Cost*quantity;
+		}else if(foodName == "Masala Puri"){
+		Cost = 55.00;
+		return Cost*quantity;
+		}else if(foodName == "Sweet Lassi"){
+		Cost = 70.00;
+		return Cost*quantity;
+		}else if(foodName == "Filter Coffee"){
+		Cost = 40.00;
+		return Cost*quantity;
+		}else if(foodName == "Plain Rava Idli"){
+		Cost = 40.00;
+		return Cost*quantity;
+		}else if(foodName == "Lemon Tea"){
+		Cost = 79.00;
+		return Cost*quantity;
+		}else if(foodName == "Small Vada"){
+		Cost = 20.00;
+		return Cost*quantity;
+		}else if(foodName == "Veg Noodles"){
+		Cost = 99.00;
+		return Cost*quantity;
+		}else if(foodName == "Veg Fried Rice"){
+		Cost = 99.00;
+		return Cost*quantity;
+		}else if(foodName == "Chilli Chicken"){
+		Cost = 95.00;
+		return Cost*quantity;
+		}else if(foodName == "Chicken Biryani"){
+		Cost = 92.00;
+		return Cost*quantity;
+		}else if(foodName == "Chicken65"){
+		Cost = 140.00;
+		return Cost*quantity;
+		}else if(foodName == "Veg Biryani"){
+		Cost = 169.00;
+		return Cost*quantity;
+		}else if(foodName == "Noodles"){
+		Cost = 120.00;
+		return Cost*quantity;
+		}else if(foodName == "Fried Rice"){
+		Cost = 130.00;
+		return Cost*quantity;
+		}else if(foodName == "Paneer Butter Masala"){
+		Cost = 150.00;
+		return Cost*quantity;
+		}else if(foodName == "Dal Makhani"){
+		Cost = 140.00;
+		return Cost*quantity;
+		}else if(foodName == "Gobi Manchurian"){
+		Cost = 130.00;
+		return Cost*quantity;
+		}else if(foodName == "Mutton Biryani"){
+		Cost = 200.00;
+		return Cost*quantity;
+		}else if(foodName == "Boiled Egg"){
+		Cost = 15.00;
+		return Cost*quantity;
+		}else if(foodName == "Thali"){
+		Cost = 150.00;
+        return Cost*quantity;		
 	    }else System.out.println(foodName + "is Invalid");
 	    return Cost;
 	    }
