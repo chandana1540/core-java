4f1f1e609cd5d2bf28ba4896be798a3e0d047395
chandana <you@example.com>
2025-07-19 18:47:24 +0530

    message

---
+++ Menu.java
@@ -0,0 +1,10 @@
+class Menu{
+	public static void menuInfo(){
+		String menuMeals = "Pure Veg";
+		String menuType = "Through MenuCard";
+		System.out.println("The Menu Meals Avilable are "+menuMeals);
+		System.out.println("The Menutype is "+menuType);
+		
+		return;
+	}
+}
---
+++ RestaurantRunner.java
@@ -0,0 +1,7 @@
+class RestaurantRunner{
+	public static void main(String chandu[]){
+		System.out.println("Main Started");
+		Restaurant.getMenuDetails();
+		System.out.println("Main Ended");
+	}
+}
