c646a11ca4f3ab6f2ea24990d8e8da866251c837
chandana <you@example.com>
2025-07-21 23:32:49 +0530

    message

---
+++ Bill.java
@@ -0,0 +1,8 @@
+class Bill {
+      String planType;
+	  boolean BillisPaid;
+	  double BillAmountWithTax;
+	  String BillDueDate;
+	  String BillExpiryDate;
+	  int BillId;
+}
--- RestaurantRunner.java
+++
@@ -1,7 +0,0 @@
-class RestaurantRunner{
-	public static void main(String chandu[]){
-		System.out.println("Main Started");
-		Restaurant.getMenuDetails();
-		System.out.println("Main Ended");
-	}
-}
